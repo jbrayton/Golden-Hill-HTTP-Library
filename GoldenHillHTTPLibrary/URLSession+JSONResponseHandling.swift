b9f8cc447273db2104fe8a7deca480d3db817dab
John Brayton <jbrayton@goldenhillsoftware.com>
2017-03-06 17:38:03 -0500

    application/json or text/json for error reader

--- GoldenHillHTTPLibrary/URLSession+JSONResponseHandling.swift
+++ GoldenHillHTTPLibrary/URLSession+JSONResponseHandling.swift
@@ -117,7 +117,7 @@ public extension URLSession {
     }
 
     private func ghs_parsePotentialError( httpResponse: HTTPURLResponse, data: Data, errorMessageInterpreter: ErrorMessageInterpreter ) -> String? {
-        guard httpResponse.mimeType == "application/json" else {
+        guard httpResponse.mimeType == "application/json" || httpResponse.mimeType == "text/json" else {
             return nil
         }
         guard let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []) else {
