f282e50e398811bb2b5d44b769d9523bbb96321f
John Brayton <jbrayton@goldenhillsoftware.com>
2017-05-13 19:11:17 -0400

    mac target

---
+++ GoldenHillHTTPLibrary-Mac/GoldenHillHTTPLibrary-Mac.h
@@ -0,0 +1,19 @@
+//
+//  GoldenHillHTTPLibrary-Mac.h
+//  GoldenHillHTTPLibrary-Mac
+//
+//  Created by John Brayton on 5/13/17.
+//  Copyright © 2017 John Brayton. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GoldenHillHTTPLibrary-Mac.
+FOUNDATION_EXPORT double GoldenHillHTTPLibrary_MacVersionNumber;
+
+//! Project version string for GoldenHillHTTPLibrary-Mac.
+FOUNDATION_EXPORT const unsigned char GoldenHillHTTPLibrary_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoldenHillHTTPLibrary_Mac/PublicHeader.h>
+
+
--- updatecarthage.sh
+++ updatecarthage.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 carthage update --no-use-binaries --platform iOS
+carthage update --no-use-binaries --platform Mac
