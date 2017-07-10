92d96f38d8ccd305c43ad003024f2499f14c27ff
John Brayton <jbrayton@goldenhillsoftware.com>
2017-07-10 18:06:52 -0400

    reinstated

--- GoldenHillHTTPLibrary/URLSession+JSONResponseHandling.swift
+++ GoldenHillHTTPLibrary/URLSession+JSONResponseHandling.swift
@@ -54,10 +54,10 @@ public extension URLSession {
        parameter.
      * It will accept an HTTP response code of 200, 201, 202, or 204.
      */
-//    public func ghs_dataTask( request: URLRequest, apiLabel: String, operationLabel: String, errorMessageInterpreter: @escaping ErrorMessageInterpreter = { (x) in return nil }, handler: @escaping HTTPAPIResultHandler<Void> ) -> URLSessionDataTask {
-//
-//        return self.ghs_dataTask(request: request, apiLabel: apiLabel, operationLabel: operationLabel, jsonResponseInterpreter: URLSession.self.ghs_voidJsonInterpreter, handler: handler)
-//    }
+    public func ghs_dataTask( request: URLRequest, apiLabel: String, operationLabel: String, errorMessageInterpreter: @escaping ErrorMessageInterpreter = { (x) in return nil }, handler: @escaping HTTPAPIResultHandler<Void> ) -> URLSessionDataTask {
+
+        return self.ghs_dataTask(request: request, apiLabel: apiLabel, operationLabel: operationLabel, jsonResponseInterpreter: URLSession.self.ghs_voidJsonInterpreter, handler: handler)
+    }
     
     /*
         A placeholder function that returns void, to be used when there is no need
